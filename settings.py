aff3e201370f70bafbd5cb8f82332496ab1dad0a
Fransenson <franz@mlchr.de>
2018-05-16 02:04:11 +0200

    Add files via upload
    example settings

---
+++ settings.py
@@ -0,0 +1,10 @@
+#Your Telegram Bot API Token in quotes! (".....")(ask @BotFather on Telegram to get one)
+api_token   = "yourAPItoken"
+
+
+#Your chat id (no quotes!)
+chat_id     = 123456
+
+#Your PT instances. Change the name for your instance and set the path to its "ProfitTrailerData.json".
+#Multiple Instances: Add new instance by copying instance 0, renaming it to 1 and separate both with a comma. For detailed instructions see the README.md
+bots    = {0: {"name": "Instance 1 Name", "path": "/path/to/ProfitTrailerData.json"}}
